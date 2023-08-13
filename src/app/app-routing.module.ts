627296302d1d546f1dbe3c68a2ddb87364539cff
shubhamchandak <shubhamchandak8@gmail.com>
2023-08-13 17:52:12 -0400

    fix routing issue on ngix deployment server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
